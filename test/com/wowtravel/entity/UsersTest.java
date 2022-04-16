5e675cfeada08dbd25897598501b8a7261e26b9d
Iryna <87706072+dev-frogdev@users.noreply.github.com>
2022-04-16 09:12:43 +0300

    Update UsersTest.java

--- test/com/wowtravel/entity/UsersTest.java
+++ test/com/wowtravel/entity/UsersTest.java
@@ -8,12 +8,12 @@ import com.wowtravel.entity.Users;
 
 public class UsersTest {
 
-
 	public static void main(String[] args) {
 		Users user1 = new Users();
 		user1.setEmail("privet@java.ua");
 		user1.setFullName("Iryna");
 		user1.setPassword("helloworld");
+		
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("WOWTravel");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 
